70109001c587fd280735bbf7b5d152f730b44b97
bitfishers <ysk1122.dev@gmail.com>
2022-04-27 20:25:06 +0900

    更新頻度の高い部位を別ファイルへ分離する

---
+++ js/sync.js
@@ -0,0 +1,9 @@
+put_introducetext() {
+    let request = XMLHttpRequest();
+    request.open("GET", "./assets_docs.txt", true);
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            
+        }
+    }
+}
