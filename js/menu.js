f24953f8a5af5ec2fa951bcf75d2f8e1620edd80
bitfishers <ysk1122.dev@gmail.com>
2022-05-22 04:11:50 +0900

    分離したファイルを書き出す機能を構築

--- js/menu.js
+++ js/menu.js
@@ -21,3 +21,13 @@ $("#barger-menu-list li a").click(function () {
   $("#barger-menu-list").removeClass('panelactive');
 });
 
+$(function(){
+  $('a[href^=#]').click(function() {
+  var speed = 500; // スクロール速度(ミリ秒)
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('html').animate({scrollTop:position}, speed, 'swing');
+  return false;
+  });
+});
--- js/sync.js
+++ js/sync.js
@@ -1,9 +1,61 @@
-put_introducetext() {
-    let request = XMLHttpRequest();
-    request.open("GET", "./assets_docs.txt", true);
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            
+const file_desc01 = "../assets_docs/introduce.txt";
+const file_gal01 = "../assets_docs/gallery-part.xml";
+
+function desc01() {
+    let req = new XMLHttpRequest();
+    req.open("GET", file_desc01, true);
+    req.send();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            const desc01 = document.getElementById("desc01");
+            desc01.innerHTML = req.responseText;
         }
     }
 }
+
+function gal() {
+    let req = new XMLHttpRequest();
+    req.open("GET", file_gal01, true);
+    req.send();
+    req.onreadystatechange = function () {
+        const gal = document.getElementById("gal");
+        if (req.readyState == 4 && req.status == 200) {
+            let docElem = req.responseXML.documentElement;
+            let data_nodes = docElem.getElementsByTagName("data");
+            let file_nodes = docElem.getElementsByTagName("file");
+            let desc_nodes = docElem.getElementsByTagName("desc");
+            for (i = 0; i < data_nodes.length; i++) {
+                let new_li = document.createElement('li');
+                let new_a = document.createElement('a');
+                let new_img = document.createElement('img');
+                    new_img.src = "./assets/" + file_nodes[i].textContent;
+                    new_a.appendChild(new_img);
+                    new_li.appendChild(new_a);
+                let new_p = document.createElement('p');
+                let desc_text = desc_nodes[i].textContent;
+                    new_p.innerHTML = desc_text.replace(/\r?\n/g, '<br>');
+                    new_li.appendChild(new_p);
+                gal.appendChild(new_li);
+            }
+            if (window.innerWidth > 1400 && data_nodes.length % 2 == 1) {
+                // 要素数が奇数のとき、最終行に空欄追加
+                let new_li = document.createElement('li');
+                let new_img = document.createElement('img');
+                let new_a = document.createElement('a');
+                    new_a.appendChild(new_img);
+                    new_li.appendChild(new_a);
+                let new_p = document.createElement('p');
+                    new_p.innerHTML = "Don't miss it.";
+                    new_li.appendChild(new_p);
+                gal.appendChild(new_li);
+            }
+        }
+    }
+}
+
+(function () {
+    const btn = document.getElementById("readmore_button");
+    btn.onclick = function () {
+        
+    }
+})();
