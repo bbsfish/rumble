cb696ecbf58800764604e45c959bb0f9bdace654
bbsfish <ysk1122.dev@gmail.com>
2023-02-23 09:05:03 +0900

    ランキング表示システム メッセージフォームシステム 作成

--- out.php
+++ out.php
@@ -3,12 +3,14 @@ $r = array();
 for ($i=0; $i < 36; $i++) { 
     $x = str_pad((string) $i+1, 3, "0", STR_PAD_LEFT);
     $arr = array(
+        "id" => "S" . $x,
         "file" => "goods_$x.jpg",
         "name" => "GOODS NAME $x",
         "desc" => "SAMPLE DESC $x",
         "price" => 1000,
         "stock" => 10,
-        "sold" => 1
+        "sold" => 1,
+        "orderform"=> "https://docs.google.com/forms/d/e/1FAIpQLSfaDj51OBVD2YUfKay23nVsDuGOs4iEPuprRqhh-mRmcVZLfw/viewform?usp=sf_link"
     );
     // $a = "<data><file>goods_$x.jpg</file><name>GOODS NAME $x</name><desc>SAMPLE DESC $x</desc><price>1000</price><stock>10<stock></data>".PHP_EOL;
     array_push($r, $arr);
