a7b9990a20a7d4f6395be73e65870891c475715d
bbsfish <ysk1122.dev@gmail.com>
2023-03-02 18:55:48 +0900

    index.html 背景画像のスクロール

---
+++ js/parallax.js
@@ -0,0 +1,22 @@
+const elem = document.querySelector("#wallpaper"); // IMG:wallpaper
+
+const allHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+);
+
+const elemHeight = elem.offsetHeight;
+const maxScrollVolume = elemHeight - window.innerHeight;
+
+document.addEventListener("scroll", function(){
+    const currentScrollVolume = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollVolume >= maxScrollVolume) {
+        // elem.style.top = (currentScrollVolume - maxScrollVolume) + "px";
+        elem.style.display = "none";
+        document.body.style.backgroundImage = "url(./lib/wallpaper.jpg)";
+    } else if (currentScrollVolume < maxScrollVolume) {
+        elem.style.display = "block";
+        document.body.style.backgroundImage = "";
+    }
+});
