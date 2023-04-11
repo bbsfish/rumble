dec9073c706eecacc5b130b56d9b6748a09c8d63
bbsfish <ysk1122.dev@gmail.com>
2023-04-11 18:41:11 +0900

    background img の調整

--- js/parallax copy.js
+++ js/parallax copy.js
@@ -1,15 +1,18 @@
+const elem = document.querySelector("#wallpaper"); // IMG:wallpaper
+
 const allHeight = Math.max(
     document.body.scrollHeight, document.documentElement.scrollHeight,
     document.body.offsetHeight, document.documentElement.offsetHeight,
     document.body.clientHeight, document.documentElement.clientHeight
 );
-// const imgHeight = elem.offsetHeight;
-const imgHeight = 1000;
+
+const imgHeight = elem.offsetHeight;
 // const maxScrollVolume = elemHeight - window.innerHeight;
 const rate = imgHeight/allHeight;
 console.log(allHeight);
 console.log(rate);
 document.addEventListener("scroll", function(){
     const currentScrollVolume = window.pageYOffset || document.documentElement.scrollTop;
-    document.body.style.backgroundPositionY = -1 * Math.floor(currentScrollVolume * rate * 0.5) + "px";
+    // console.log(currentScrollVolume);
+    elem.style.top = (currentScrollVolume * rate) + "px";
 });
