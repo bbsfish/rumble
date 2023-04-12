141065b2f4553e10805cf46f5edbcf0c83b3e053
bbsfish <ysk1122.dev@gmail.com>
2023-04-12 21:47:14 +0900

    difference on desktop

--- js/parallax.js
+++ js/parallax.js
@@ -8,15 +8,19 @@ const allHeight = Math.max(
 
 const elemHeight = elem.offsetHeight;
 const maxScrollVolume = elemHeight - window.innerHeight;
+console.log(maxScrollVolume);
+    document.addEventListener("scroll", function(){
+        const currentScrollVolume = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScrollVolume >= maxScrollVolume) {
+            // elem.style.top = (currentScrollVolume - maxScrollVolume) + "px";
+            elem.style.display = "none";
+            document.body.style.backgroundImage = "url(./lib/wallpaper.jpg)";
+        } else if (currentScrollVolume < maxScrollVolume) {
+            elem.style.display = "block";
+            document.body.style.backgroundImage = "";
+        }
+    });
 
-document.addEventListener("scroll", function(){
-    const currentScrollVolume = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScrollVolume >= maxScrollVolume) {
-        // elem.style.top = (currentScrollVolume - maxScrollVolume) + "px";
-        elem.style.display = "none";
-        document.body.style.backgroundImage = "url(./lib/wallpaper.jpg)";
-    } else if (currentScrollVolume < maxScrollVolume) {
-        elem.style.display = "block";
-        document.body.style.backgroundImage = "";
-    }
-});
+// document.addEventListener("scroll", function(){
+//     console.log(elem.offsetHeight);
+// });
